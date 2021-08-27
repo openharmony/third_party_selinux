d6889909bcfa9e1221c2d86613126a2692d9c7d3
Qin Fandong <qinfd@superred.com.cn>
2021-08-27 12:30:05 +0800

    更新了 fts 和 pcre2
    (BSD) depends/fts/fts.h    <- https://github.com/openbsd/src/blob/e8835b178a3e9df00c1c1fe0b9875fc5ef5a7854/include/fts.h
    (BSD) depends/fts/fts.c    <- https://github.com/openbsd/src/blob/e8835b178a3e9df00c1c1fe0b9875fc5ef5a7854/lib/libc/gen/fts.c
    (BSD) depends/pcre/pcre2/  <- https://github.com/PhilipHazel/pcre2/files/7027195/pcre2-10.37.tar.gz
    
    Signed-off-by: Qin Fandong <qinfd@superred.com.cn>

--- depends/pcre/include/pcre2.h
+++ depends/pcre/include/pcre2.h
@@ -1,10 +1,7 @@
-
 #ifndef _INCLUDE_PCRE2_H_
 #define _INCLUDE_PCRE2_H_
 
 #define PCRE2_CODE_UNIT_WIDTH 8
-
-#include "../dist2/src/pcre2.h.generic"
-
+#include "../pcre2/src/pcre2.h.generic"
 
 #endif // _INCLUDE_PCRE2_H_
