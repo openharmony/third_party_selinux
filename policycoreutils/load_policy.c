6ab48f79f3c812808a34b9034b8735929429656d
Qin Fandong <qinfd@superred.com.cn>
2021-09-03 17:14:06 +0800

    修复编译错误
    Signed-off-by: Qin Fandong <qinfd@superred.com.cn>

--- policycoreutils/load_policy.c
+++ policycoreutils/load_policy.c
@@ -9,14 +9,14 @@ int load_policy(void) {
   if ((ret = selinux_init_load_policy(&enforce))) {
     if (enforce > 0) {
       fprintf(stderr,
-              "%s:  Can't load policy and enforcing mode requested:  %s\n",
-              argv[0], strerror(errno));
+              "Can't load policy and enforcing mode requested:  %s\n",
+              strerror(errno));
       return -1;
     }
   }
 
   if ((ret = selinux_mkload_policy(0)) < 0) {
-    fprintf(stderr, "%s:  Can't load policy:  %s\n", argv[0], strerror(errno));
+    fprintf(stderr, "Can't load policy:  %s\n", strerror(errno));
     return -1;
   }
 
