840e6f3b0cf86ff42e0a462b523983a52ec4c8a2
Xie Bo <xieb@superred.com.cn>
2021-09-10 02:59:30 +0000

    update generate policy.31
    Signed-off-by: Xie Bo <xieb@superred.com.cn>

--- demo/push_precompiled_sepolicy.bat
+++
@@ -1,8 +0,0 @@
-hdc_std.exe shell mkdir /etc/selinux
-hdc_std.exe shell mkdir /etc/selinux/targeted
-hdc_std.exe shell mkdir /etc/selinux/targeted/policy
-hdc_std.exe shell mkdir /etc/selinux/targeted/contexts
-hdc_std.exe shell mkdir /etc/selinux/targeted/contexts/files
-hdc_std.exe file send precompiled_sepolicy /etc/selinux/targeted/policy/policy.31
-hdc_std.exe file send file_contexts /etc/selinux/targeted/contexts/files/file_contexts
-pause
